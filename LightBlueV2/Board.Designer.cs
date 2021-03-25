ac2a9c33b12faae592ddb077b33f6f3afd7c04d8
Alexander Molotkov <molotkoa@oregonstate.edu>
2021-03-24 19:01:54 -0700

    Creates empty chess board by implementing paint event handler.

--- LightBlueV2/Board.Designer.cs
+++ LightBlueV2/Board.Designer.cs
@@ -33,9 +33,12 @@ namespace LightBlueV2
             // 
             // Board
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(679, 681);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximumSize = new System.Drawing.Size(695, 720);
+            this.MinimumSize = new System.Drawing.Size(695, 720);
             this.Name = "Board";
             this.Text = "Board";
             this.Load += new System.EventHandler(this.Board_Load);
--- LightBlueV2/Board.cs
+++ LightBlueV2/Board.cs
@@ -17,9 +17,39 @@ namespace LightBlueV2
             InitializeComponent();
         }
 
+        private PictureBox PB = new PictureBox();
+
         private void Board_Load(object sender, EventArgs e)
         {
+            PB.Dock = DockStyle.Fill;
+            PB.BackColor = Color.PapayaWhip;
+            PB.Paint += new System.Windows.Forms.PaintEventHandler(this.Board_Draw);
 
+            this.Controls.Add(PB);
         }
+
+        private void Board_Draw(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            Brush b = new SolidBrush(Color.Tan);
+
+            int SquareWidth = (PB.Height / 8);
+
+            int Rx;
+            int Ry;
+
+            for(int i = 0; i < 8; i++)
+            {
+                for(int j = 0; j < 8; j++)
+                {
+                    if((j+i) % 2 == 1)
+                    {
+                        Rx = i * SquareWidth;
+                        Ry = j * SquareWidth;
+                   
+                        e.Graphics.FillRectangle(b, new Rectangle(Rx, Ry, SquareWidth+1, SquareWidth+1));
+                    }
+                }
+            }
+        }   
     }
 }
