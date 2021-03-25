ccd638c849f0056b32081ee1aad3b79fd1f6632c
Alexander Molotkov <molotkoa@oregonstate.edu>
2021-03-24 21:03:38 -0700

    Handles turn logic, populates board, some minor changes to piece classes

--- LightBlueV2/Game.cs
+++ LightBlueV2/Game.cs
@@ -2,59 +2,106 @@
 using System.Drawing;
 using System.Windows.Forms;
 
-
 namespace LightBlueV2
-{ 
+{
 
-	// TODO: PUT THIS IN BOARD
-	// Start a drag of a piece.
-	public class MoveForm : Form {
-		private PictureBox pieceDragSource;
-		private PictureBox pieceDropTarget;
-		private void MoveForm_Load(object sender, EventArgs e)
-		{
-			pieceDropTarget.AllowDrop = true;
-		}
-		private void Piece_Load(object sender, EventArgs e)
-		{
-			pieceDropTarget.AllowDrop = true;
-		}
-		private void pieceDragSource_MouseDown(object sender,
-			MouseEventArgs e)
-		{
-			// Start the drag if it's the left mouse button.
-			if (e.Button == MouseButtons.Left)
-			{
-				pieceDragSource.DoDragDrop(pieceDragSource.Image,
-					DragDropEffects.Move);
-			}
-		}
-		// Accept the drop.
-		private void pieceDropTarget_DragDrop(object sender,
-			DragEventArgs e)
-		{
-			pieceDropTarget.Image =
-				(Bitmap)e.Data.GetData(DataFormats.Bitmap, true);
-		}
-	}
+	public struct Move
+    {
+		public int fromRow;
+		public int fromCol;
+		public int toRow;
+		public int toCol;
+    }
 
 	public class Game
 	{
-		public string status;
-		public string player;
-		public void GameLoop()
-		{
-			while (status != "finished")
-			{
-				Turn(player);
-			}
-		}
-		public void Turn()
-		{
-			if (player == "white")
-			{
 
+		Piece[,] Board = new Piece[8, 8];
+		private bool HasMoved { get; set; }
+		private Move PlayerMove { get; set; }
+		private bool EndGame { get; set; }
+		private int TurnNum { get; set; }
+
+
+        public void GameLoop()
+		{
+
+			PopulateBoard(this.Board); 
+			TurnNum = 0;
+
+			while (!EndGame)
+			{
+				//Turn();
 			}
+			System.Console.WriteLine("Game Over!");
+		}
+		private void Turn()
+		{
+			while (HasMoved == false)
+            {
+				//Wait for a move
+			}
+
+			if (ValidateMove(PlayerMove, this.Board))
+			{
+				TurnNum++;
+				//TODO:
+				//RecordMove(PlayerMove)
+				MakeMove(PlayerMove, this.Board);
+				
+			}
+			HasMoved = false;
+		}
+
+		private bool ValidateMove(Move Mv, Piece[,] Board)
+        {
+			//TODO
+			return true;
+        }
+
+		private void MakeMove(Move mv, Piece [,] Board)
+        {
+			Board[mv.toRow, mv.toCol] = Board[mv.fromRow, mv.fromCol];
+			Board[mv.fromRow, mv.fromCol] = null;
+			return;
+        }
+
+		public int PopulateBoard(Piece[,] board)
+		{
+			for(int i = 0; i < 8; i++)
+            {
+				for(int j = 0; j < 8; j++)
+                {
+					Board[i, j] = null;
+                }
+            }
+
+			board[0,0] = new Rook('B');
+			board[0,7] = new Rook('B');
+			board[7,0] = new Rook('W');
+			board[7,7] = new Rook('W');
+			board[0,1] = new Knight('B');
+			board[0,6] = new Knight('B');
+			board[7,1] = new Knight('W');
+			board[7,6] = new Knight('W');
+			board[0,2] = new Bishop('B');
+			board[0,5] = new Bishop('B');
+			board[7,2] = new Bishop('W');
+            board[7,5] = new Bishop('W');
+			board[0,3] = new Queen('B');
+			board[7,3] = new Queen('W');
+			board[0,4] = new King('B');
+			board[7,4] = new King('W');
+
+			for (int i = 0; i < 8; i++)
+			{
+				board[1,i] = new Pawn('B');
+			}
+			for (int i = 0; i < 8; i++)
+			{
+				board[6,i] = new Pawn('W');
+			}
+			return 0;
 		}
 	}
 }
--- LightBlueV2/Pieces.cs
+++ LightBlueV2/Pieces.cs
@@ -4,77 +4,54 @@ using System.Windows.Forms;
 
 namespace LightBlueV2
 {
+	public class Piece
+	{
+		public char color { get; set; }
+
+		public Piece()
+        {
+
+        }
+
+		public Piece(char color)
+        {
+			this.color = color;
+        }
+	}
 	public class Bishop : Piece
 	{
+        public Bishop(char color) : base(color)
+        {
+        }
 	}
 	public class Knight : Piece
 	{
+        public Knight(char color) : base(color)
+        {
+        }
 	}
-	public class Rook : Piece
+    public class Rook : Piece
+    {
+        public Rook(char color) : base(color)
+        {
+        }
+    }
+    public class King : Piece
 	{
-	}
-	public class King : Piece
-	{
+        public King(char color) : base(color)
+        {
+        }
 	}
 	public class Pawn : Piece
 	{
-	}
-	public class Piece
-	{
-		public string color { get; set; }
+        public Pawn(char color) : base(color)
+        {
+        }
 	}
 	public class Queen: Piece
 	{
-	}
-
-	public class Setup
-	{
-		public int PopulateBoard(Piece[][] board)
-		{
-			Rook piece = new Rook();
-			piece.color = "black";
-			board[0][0] = piece;
-			board[0][7] = piece;
-			piece.color = "white";
-			board[7][0] = piece;
-			board[7][7] = piece;
-			Knight piecek = new Knight();
-			piecek.color = "black";
-			board[0][1] = piecek;
-			board[0][6] = piecek;
-			piecek.color = "white";
-			board[7][1] = piecek;
-			board[7][6] = piecek;
-			Bishop pieceb = new Bishop();
-			piece.color = "black";
-			board[0][2] = pieceb;
-			board[0][5] = pieceb;
-			pieceb.color = "white";
-			board[7][2] = pieceb;
-			board[7][5] = pieceb;
-			Queen pieceq = new Queen();
-			pieceq.color = "black";
-			board[0][3] = pieceq;
-			pieceq.color = "white";
-			board[7][3] = pieceq;
-			King piecei = new King();
-			piecei.color = "black";
-			board[0][4] = piecei;
-			piecei.color = "white";
-			board[7][4] = piecei;
-
-			Pawn piecep = new Pawn();
-			piecep.color = "black";
-			for (int i = 0; i < 8; i++)
-			{
-				board[1][i] = piecep;
-			}
-			piecep.color = "white";
-			for (int i = 0; i < 8; i++)
-			{
-				board[6][i] = piecep;
-			}
-			return 0;
-		}
+        public Queen(char color) : base(color)
+        {
+        }
 	}
 }
