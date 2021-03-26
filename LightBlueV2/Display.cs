f79a8c47be37bc74e1ccca0fbd27eb9c8ffef442
cmhekkala <cmhekkala@gmail.com>
2021-03-25 20:50:53 -0700

    Adds custom cursor and snapback for incorrect moves.

--- LightBlueV2/Display.cs
+++ LightBlueV2/Display.cs
@@ -89,6 +89,7 @@ namespace LightBlueV2
                         pbs[i,j].MouseDown += pieceDragSource_MouseDown;
                         pbs[i,j].DragEnter += pieceDropTarget_DragEnter;
                         pbs[i,j].DragDrop += pieceDropTarget_DragDrop;
+                        pbs[i, j].GiveFeedback += pieceDragSource_GiveFeedback;
                         pbs[i,j].Location = new Point(x_coord, y_coord);
                         pbs[i,j].BackColor = Color.Transparent;
                         pbs[i, j].AllowDrop = true;
@@ -147,15 +148,27 @@ namespace LightBlueV2
                     PictureBox pb = (PictureBox)sender;
                     if (pb.Image != null)
                     {
-                        Bitmap bm = new Bitmap(pb.Image);
+                        Image tempImage = pb.Image;
+                        pb.Image = null;
+                        Bitmap bm = new Bitmap(tempImage);
                         IntPtr Hicon = bm.GetHicon();
                         dragCursor = new Cursor(Hicon);
-                        
-                        pb.DoDragDrop(pb.Image,
+                        Cursor.Current = dragCursor;
+                        DragDropEffects drop = pb.DoDragDrop(tempImage,
                             DragDropEffects.Move);
-                        pb.Image = null;
+                        if (drop == DragDropEffects.None)
+                        {
+                            pb.Image = tempImage;
+                        }
                     }
                 }
+            }
+            private void pieceDragSource_GiveFeedback(object sender, GiveFeedbackEventArgs e)
+            {
+                // Use custom cursor.
+                // Sets the custom cursor based upon the effect.
+                e.UseDefaultCursors = false;
+                Cursor.Current = dragCursor;
             }
             // Allow a move of an image.
             private void pieceDropTarget_DragEnter(object sender,
