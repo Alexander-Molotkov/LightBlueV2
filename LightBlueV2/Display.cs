9b68f4bb95e1238c6e69013d1ead581f79d8e623
cmhekkala <cmhekkala@gmail.com>
2021-03-30 18:47:51 -0700

    Adds potential functionality for resizing custom cursor.

--- LightBlueV2/Display.cs
+++ LightBlueV2/Display.cs
@@ -10,6 +10,76 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// https://stackoverflow.com/questions/24695976/resize-system-icon-in-c-sharp
+public class IconEx : IDisposable
+{
+    public enum SystemIcons
+    {
+        Application = 100,
+        Asterisk = 104,
+        Error = 103,
+        Exclamation = 101,
+        Hand = 103,
+        Information = 104,
+        Question = 102,
+        Shield = 106,
+        Warning = 101,
+        WinLogo = 100
+    }
+
+    // Edited to pass in hicon instead of path
+    public IconEx(IntPtr hicon, Size size)
+    {
+        if (hicon == IntPtr.Zero) throw new System.ComponentModel.Win32Exception();
+        attach(hicon);
+
+    }
+    public IconEx(SystemIcons sysicon, Size size)
+    {
+        IntPtr hUser = GetModuleHandle("user32");
+        IntPtr hIcon = LoadImage(hUser, (IntPtr)sysicon, IMAGE_ICON, size.Width, size.Height, 0);
+        if (hIcon == IntPtr.Zero) throw new System.ComponentModel.Win32Exception();
+        attach(hIcon);
+    }
+
+
+    public Icon Icon
+    {
+        get { return this.icon; }
+    }
+
+    public void Dispose()
+    {
+        if (icon != null) icon.Dispose();
+    }
+
+    private Icon icon;
+
+    private void attach(IntPtr hIcon)
+    {
+        // Invoke the private constructor so we can get the Icon object to own the handle
+        var ci = typeof(Icon).GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance,
+            null, new Type[] { typeof(IntPtr), typeof(bool) }, null);
+        this.icon = (Icon)ci.Invoke(new object[] { hIcon, true });
+    }
+
+    private const int IMAGE_ICON = 1;
+    private const int LR_LOADFROMFILE = 0x10;
+    private const int LR_SHARED = 0x8000;
+
+    [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    private static extern IntPtr GetModuleHandle(string name);
+    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    private static extern IntPtr LoadImage(IntPtr hinst, string lpszName, int uType,
+                                 int cxDesired, int cyDesired, int fuLoad);
+    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    private static extern IntPtr LoadImage(IntPtr hinst, IntPtr resId, int uType,
+                                 int cxDesired, int cyDesired, int fuLoad);
+}
+
 namespace LightBlueV2
 {
     public partial class Display : Form
@@ -94,7 +164,7 @@ namespace LightBlueV2
                         pbs[i,j].DragEnter += pieceDropTarget_DragEnter;
                         pbs[i,j].DragDrop += pieceDropTarget_DragDrop;
                         pbs[i, j].GiveFeedback += pieceDragSource_GiveFeedback;
-                        pbs[i,j].Location = new Point(x_coord, y_coord);
+                        pbs[i,j].Location = new System.Drawing.Point(x_coord, y_coord);
                         pbs[i,j].BackColor = Color.Transparent;
                         pbs[i, j].AllowDrop = true;
                         this.Controls.Add(pbs[i,j]);
@@ -143,17 +213,24 @@ namespace LightBlueV2
                         IntPtr Hicon = bm.GetHicon();
                         dragCursor = new Cursor(Hicon);
                         Cursor.Current = dragCursor;
-                        // Graphics graphics = this.CreateGraphics();
-                        // Rectangle rectangle = new Rectangle(
-                          // new Point(10, 10), new Size((int)Math.Floor(SquareWidth),
-                          // (int)Math.Floor(SquareWidth)));
-                        // dragCursor.Draw(graphics, rectangle);
+
+                        // This chunk is non-functional, but let's see if it does something eventually.
+                        Graphics graphics = this.CreateGraphics();
+                        Rectangle rectangle = new Rectangle(
+                          new Point(10, 10), new Size(Cursor.Size.Width/2,
+                          (int)Math.Floor(SquareWidth)));
+                        dragCursor.DrawStretched(graphics, rectangle);
                         DragDropEffects drop = pb.DoDragDrop(tempImage,
                             DragDropEffects.Move);
+                        var icon = new IconEx(Hicon, new Size(10, 10));
+
+                        Cursor.Current = dragCursor;
                         if (drop == DragDropEffects.None)
                         {
                             pb.Image = tempImage;
                         }
+                        dragCursor.Dispose();
+                        Cursor.Current = Cursors.Default;
                     }
                 }
             }
